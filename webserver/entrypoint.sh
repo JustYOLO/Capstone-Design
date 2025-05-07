f20c0651f24492a4f16104dd06a60ac6b2d12739
lee-lab <nascarf16@dankook.ac.kr>
2025-05-07 18:15:18 +0900

    fix: undo --fake migration

--- webserver/entrypoint.sh
+++ webserver/entrypoint.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 set -e
 
-python manage.py makemigrations accounts
-
-# 1) Fake the initial migration for your "accounts" app  
-python manage.py migrate accounts 0001_initial --fake
-
 python manage.py migrate --noinput
 exec gunicorn --bind 0.0.0.0:8000 --workers 3 webserver.wsgi:application
