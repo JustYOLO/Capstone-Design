de906b0ad211ed8755c9639651dc3980dbca3a35
lee-lab <nascarf16@dankook.ac.kr>
2025-05-12 16:32:19 +0900

    fix: CSRF_TRUSTED_ORIGINS url fix

--- webserver/webserver/settings.py
+++ webserver/webserver/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = bool(os.environ.get("DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
-CSRF_TRUSTED_ORIGINS = os.environ.get("DJANGO_ALLOWED_HOSTS","127.0.0.1").split(",")
+CSRF_TRUSTED_ORIGINS = ['https://blossompick.duckdns.org']
 
 # Application definition
 
