8616b84d25add48b89a6bba8825482b68ae6f47a
lee-lab <nascarf16@dankook.ac.kr>
2025-06-08 15:27:56 +0900

    fix: media url location

--- webserver/webserver/urls.py
+++ webserver/webserver/urls.py
@@ -94,4 +94,12 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=os.path.join(BASE_DIR, 'webserver/static/build/static'))
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(
+        r"^media/(?P<path>.*)$",
+        serve,
+        {"document_root": settings.MEDIA_ROOT},
+    ),
+]
