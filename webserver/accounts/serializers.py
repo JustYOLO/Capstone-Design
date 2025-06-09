0f43a18179aa705e41b40a658669bfc0ef479b8d
lee-lab <nascarf16@dankook.ac.kr>
2025-06-09 21:23:37 +0900

    fix: post username instead of name

--- webserver/accounts/serializers.py
+++ webserver/accounts/serializers.py
@@ -12,9 +12,6 @@ from .models import BusinessProfile, Order
 User = get_user_model()
 
 class CustomRegisterSerializer(RegisterSerializer):
-
-    name = serializers.CharField(max_length=150, required=True)
-
     NORMAL   = User.NORMAL
     BUSINESS = User.BUSINESS
     USER_TYPE_CHOICES = User.USER_TYPE_CHOICES
@@ -31,16 +28,8 @@ class CustomRegisterSerializer(RegisterSerializer):
 
     def get_cleaned_data(self):
         data = super().get_cleaned_data()
-        data['name'] = self.validated_data.get('name', '')
         data["user_type"] = self.validated_data.get("user_type")
         return data
-
-    def save(self, request):
-        user = super().save(request) 
-        user.first_name = self.cleaned_data['name']
-        user.save()
-        return user
-
 
 class BusinessRegisterSerializer(RegisterSerializer):
     file = serializers.FileField(write_only=True)
